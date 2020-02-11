08162750725a4e34c49dbd61ef1d7e5c2d3329e2
pawelgrzeda <60094765+pawelgrzeda@users.noreply.github.com>
2020-02-11 13:25:49 +0100

    Update Main.java
    version print

--- src/main/java/com/filip/Main.java
+++ src/main/java/com/filip/Main.java
@@ -3,7 +3,8 @@ package com.filip;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String version = System.getProperty("java.version");
+        System.out.println("Hello World in version "+version);
     }
 
 }
